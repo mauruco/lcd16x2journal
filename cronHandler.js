ef0848f976cd1e22d943ec1bb25fda80ae3b1a5b
Mauro Brizida <mauruco@gmail.com>
2019-02-16 16:10:25 -0200

    Gmail Journal Added

--- cronHandler.js
+++ cronHandler.js
@@ -1,16 +1,13 @@
 const fs = require('fs');
 const { exec } = require('child_process');
 
-/*
-# CRONTAB
-@reboot node /home/pi/lcd/app &> /dev/null &
-0,15 * * * * node /home/pi/lcd/cronHandler &> /dev/null
-*/
+// CRONTAB
+// @reboot node /home/pi/lcd/app &> /dev/null &
+// */15 * * * * node /home/pi/lcd/cronHandler &>> /home/pi/cron.log
 
 const getApis = () => {
   
   return new Promise((resolve, reject) => {
-    
     fs.readdir(`${__dirname}/apis`, (error, apis) => { resolve(apis) });
   });
 }
